1d13d74081bffc0076d96b54a81e026265232342
Hitesh Sakhare <99487579+HiteshSakhare@users.noreply.github.com>
2022-04-10 09:38:29 +0530

    Week4 Test Task2- Swap two number using temp var

---
+++ Week4 Test/week4Test.js
@@ -0,0 +1,8 @@
+let num1=prompt("Enter First Number: ")
+let num2=prompt("Enter Second Number: ")
+let temp=0
+console.log(`Num1= ${num1} & Num2= ${num2}`)
+temp=num1
+num1=num2
+num2=temp
+console.log(`Num1= ${num1} & Num2= ${num2}`)
